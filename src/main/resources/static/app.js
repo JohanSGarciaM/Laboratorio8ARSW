dfe3f4cb1f36fe668481df09d5d1dbb57c547bec
unknown <johan.garcia@labinfo.is.escuelaing.edu.co>
2023-03-23 09:59:11 -0500

    avance

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -32,12 +32,12 @@ var app = (function () {
         console.info('Connecting to WS...');
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
-        
+        stompClient.send("/topic/newpoint", {}, JSON.stringify({x:10,y:10})); 
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/TOPICXX', function (eventbody) {
-                
+            stompClient.subscribe('/topic/newpoint', function (eventbody) {
+                var theObject=JSON.parse(message.body);
                 
             });
         });
